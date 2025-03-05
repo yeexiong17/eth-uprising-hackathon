21b9df6c3e947808b20182104494371f3eb0d8e1
lenovo/yeexiong17 <wyeexiong17@gmail.com>
2025-03-05 16:09:20 +0800

    refactor: uncommented import for useScaffoldContract hook

--- packages/nextjs/hooks/scaffold-eth/index.ts
+++ packages/nextjs/hooks/scaffold-eth/index.ts
@@ -5,7 +5,7 @@ export * from "./useFetchBlocks";
 export * from "./useInitializeNativeCurrencyPrice";
 export * from "./useNetworkColor";
 export * from "./useOutsideClick";
-// export * from "./useScaffoldContract";
+export * from "./useScaffoldContract";
 export * from "./useScaffoldEventHistory";
 export * from "./useScaffoldReadContract";
 export * from "./useScaffoldWatchContractEvent";
